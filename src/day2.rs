9f67d3a4eb5acb29ff37e7e62d55cfd73ac825a1
Niklas Wojtkowiak <niklas@wojtkowiak.com>
2024-12-02 19:50:43 -0500

    day 2

---
+++ src/day2.rs
@@ -0,0 +1,11 @@
+/*#[aoc(day2, part1)]
+pub fn part_one(input: &str) -> u32 {
+    0
+    /// TODO: redo
+}*/
+
+/*#[aoc(day2, part2)]
+pub fn part_two(input: &str) -> u32 {
+    0
+    /// TODO: redo
+}*/
